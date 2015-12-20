3a868c3f19ece1a67f5144cda8520b9c81277787
Gunasekaran, Arjune <Arjune.Gunasekaran@Staples.com>
2015-12-19 21:44:59 -0500

    - topic-list file created to actually write code to fetch the list of topics when app loads. - file added to main.jsx

--- src/components/main.jsx
+++ src/components/main.jsx
@@ -1,12 +1,22 @@
 var React = require('react');
 
 var Header = require('./header.jsx');
+var TopicList = require('./topic-list');
 
 module.exports = React.createClass({
   render: function() {
     return <div>
       <Header />
-      {this.props.children}
+      {this.content()}
     </div>
+  },
+
+  content: function(){
+    if(this.props.children) {
+      return this.props.children
+    }
+    else {
+      return <TopicList />  
+    }
   }
 });
