785bc51617ed38b8caa80247bb6f8e727eb2fb6e
Gunasekaran, Arjune <Arjune.Gunasekaran@Staples.com>
2015-12-19 23:29:59 -0500

    Adding topics in the header using the same topic store that was created for the topic list page.

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,8 +1,26 @@
 var React = require('react');
 var Router = require('react-router');
+var Reflux = require('reflux');
+
+var Actions = require('../actions');
+var TopicStore = require('../stores/topic-store');
 var Link = Router.Link;
 
 module.exports = React.createClass({
+  mixins: [
+    Reflux.listenTo(TopicStore, 'onChange')
+  ],
+
+  getInitialState: function(){
+    return {
+      topics: []
+    }
+  },
+
+  componentWillMount: function(){
+    Actions.getTopics();
+  },
+
   render: function(){
     return <nav className="navbar navbar-default header">
       <div className="container-fluid">
@@ -10,9 +28,23 @@ module.exports = React.createClass({
           Imgur browser
         </Link>
         <ul className="nav navbar-nav navbar-right">
-          <li><a href="">Topic 1</a></li>
+          {this.renderItems()}
         </ul>
       </div>
     </nav>
+  },
+
+  onChange: function(event, topics){
+    this.setState({
+      topics: topics
+    });
+  },
+
+  renderItems: function(){
+    return this.state.topics.slice(0,4).map(function(topic){
+      return <li key={topic.id}>
+        <Link to={"topic/" + topic.id}>{topic.name}</Link>
+      </li>
+    });
   }
 });
